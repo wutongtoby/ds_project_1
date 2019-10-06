4979b2e125d9cd76ebb058f2f3d96ff01edb77ba
吳瞳 <wutongtoby@gmail.com>
2019-10-06 09:13:29 +0800

    trying

--- tetris.cpp
+++ tetris.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-#include <stack>
+#include <fstream>
+
+using namespace std;
 
 int main(void) {
-    std::stack <int> A;
-    
+    fstream input, output;
+    char buffer[300];
+
+    input.open("tetris.data", ios::in);
+    output.open("tetris.final", ios::out);
+
+    if (!input) {
+        cout << "can't open the file\n";
+
+    }
+    else {
+        input.read(buffer, sizeof(buffer));
+    }
+    cout << buffer<<endl;
     return 0;
     
 }
