e7815c9a32db3e95a0e7db24d48010521ce2d11a
吳瞳 <wutongtoby@gmail.com>
2019-10-06 23:30:56 +0800

    complete the carving of block-- ver1

--- tetris.cpp
+++ tetris.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-typedef struct Data {
+struct Data {
     char type[3];
     int column;
 };
@@ -12,12 +12,12 @@ class game {
     private:
         int rows, cols;
         bool** table;
-        bool status; // alive or dead
+        bool alive; // alive or dead
     public:
         // the constructor
-        game(int m, int n) : rows(m), cols(n), status(1) {
-            int* temp = int[rows * cols]
-            table = new int*[rows];
+        game(int m, int n) : rows(m + 4), cols(n), status(1) {
+            bool* temp = new bool[rows * cols];
+            table = new bool*[rows];
             for(int i = 0; i < rows; i++) {
                 table[i] = temp + cols * i;
                 for (int j = 0; j < cols; j++)
@@ -28,19 +28,127 @@ class game {
             delete [] table[0];
             delete table;
         }
-        void newdata(Data d) {
-            // will change the table to the newest form
-        }
+        void newdata(Data); // to throw new data into the game
+        void clear_horizontal(void); // to clear horizontal line on the table    
+        bool check_alive(void); // if the top of the table has block, alive = 0
         bool still_alive(void) {
-            return status;
+            return alive;
         }
-        void show_table {
-            for (int i = 0; i < rows; i++) {
+        void show_table(void) {
+            for (int i = 4; i < rows; i++) {
                 for (int j = 0; j < cols; j++)
-                    cout << table[i][j] << ' ';
+                    cout << table[i][j];
+                cout << endl;
             }
-            
         }
+};
+
+void game:: newdata(Data d) {
+    // catorgorize the block 
+    // let block fall step by step, checking if it is stucked
+    // if stucked, call clear_horizontal(), then call check_alive()
+    int block_height, block_weidth;
+    bool block[4][4] = {0};
+
+    if (strcmp(d.type, "T1") == 0) {
+        block_height = 2;
+        block_weidth = 3;
+        block[0][0] = 1, block[0][1] = 1,block[0][2] = 1;
+        block[1][1] = 1; 
+    }
+    else if (strcmp(d.type, "T2") == 0) {
+        block_height = 3;
+        block_weidth = 2;
+        block[0][1] = 1;
+        block[1][0] = 1, block[1][1] = 1;
+        block[2][1] = 1;
+    }
+    else if (strcmp(d.type, "T3") == 0) {
+        block_height = 2;
+        block_weidth = 3;
+        block[0][1] = 1;
+        block[1][0] = 1, block[1][1] = 1, block[1][2] = 1;
+    }
+    else if (strcmp(d.type, "T4") == 0) {
+        block_height = 3;
+        block_weidth = 2;
+        block[0][0] = 1;
+        block[0][1] = 1, block[1][1] = 1;
+        block[2][0] = 1;
+    }
+    else if (strcmp(d.type, "L1") == 0) {
+        block_height = 3;
+        block_weidth = 2;
+        block[0][0] = 1;
+        block[1][0] = 1;
+        block[2][0] = 1, block[2][1] = 1; 
+    }
+    else if (strcmp(d.type, "L2") == 0) {
+        block_height = 2;
+        block_weidth = 3;
+        block[0][0] = 1, block[0][1] = 1, block[0][2] = 1;
+        block[1][0] = 1;
+    }
+    else if (strcmp(d.type, "L3") == 0) {
+        block_height = 3;
+        block_weidth = 2;
+        block[0][0] = 1, block[0][1] = 1;
+        block[1][1] = 1;
+        block[2][1] = 1;
+    }
+    else if (strcmp(d.type, "L4") == 0) {
+        block_height = 2;
+        block_weidth = 3;
+        block[0][2] = 1;
+        block[1][0] = 1, block[1][1] = 1, block[1][2] = 1;
+    }
+    else if (strcmp(d.type, "J1") == 0) {
+        block_height = 3;
+        block_weidth = 2;
+        block[0][1] = 1;
+        block[1][1] = 1;
+        block[2][0] = 1, block[2][1] = 1;
+    }
+    else if (strcmp(d.type, "J2") == 0) {
+        block_height = 2;
+        block_weidth = 3;
+        block[0][0] = 1;
+        block[1][0] = 1, block[1][1] = 1, block[1][2] = 1;
+    }
+    else if (strcmp(d.type, "J3") == 0) {
+        block_height = 3;
+        block_weidth = 2;
+        block[0][0] = 1, block[0][1] = 1;
+        block[1][0] = 1;
+        block[2][0] = 1;
+    }
+    else if (strcmp(d.type, "J4") == 0) {
+        block_height = 2;
+        block_weidth = 3;
+        block[0][0] = 1, block[0][1] = 1, block[0][2] = 1;
+        block[1][2] = 1;
+    }
+    else if (strcmp(d.type, "I1") == 0) {
+        block_height = 4;
+        block_weidth = 1;
+        block[0][0] = 1;
+        block[1][0] = 1;
+        block[2][0] = 1;
+        block[3][0] = 1;
+    }
+    else if (strcmp(d.type, "I2") == 0) {
+        block_height = 1;
+        block_weidth = 4;
+        block[0][0] = 1, block[0][1] = 1, block[0][2] = 1, block[0][3] = 1;
+    }
+    else if (strcmp(d.type, "O") == 0) {
+        block_height = 2;
+        block_weidth = 2;
+        block[0][0] = 1, block[0][1] = 1;
+        block[1][0] = 1, block[1][1] = 1;
+    }
+    else 
+        throw "the type of block dosen't exist\n";
 }
 
 int main(void) {
@@ -57,12 +165,11 @@ int main(void) {
         cin >> data[j].column;
         j++;
     }
-    game tetris(m, n);
+    game tetris(m, n); /*
     while (tetris.still_alive() && i < j) {
         tetris.newdata(data[i++]);
     }
-    
-    
-
+    */
+    tetris.show_table();
     return 0;    
 }
