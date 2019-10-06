fc60ec78c6b8debe84f7da0d19ac1e477e04d7f7
吳瞳 <wutongtoby@gmail.com>
2019-10-06 14:10:56 +0800

    prototype

--- tetris.cpp
+++ tetris.cpp
@@ -1,23 +1,68 @@
 #include <iostream>
-#include <fstream>
+#include <stdio.h>
+#include <string.h>
 
 using namespace std;
 
+typedef struct Data {
+    char type[3];
+    int column;
+};
+class game {
+    private:
+        int rows, cols;
+        bool** table;
+        bool status; // alive or dead
+    public:
+        // the constructor
+        game(int m, int n) : rows(m), cols(n), status(1) {
+            int* temp = int[rows * cols]
+            table = new int*[rows];
+            for(int i = 0; i < rows; i++) {
+                table[i] = temp + cols * i;
+                for (int j = 0; j < cols; j++)
+                    table[i][j] = 0;
+            }
+        }
+        ~game(void) {
+            delete [] table[0];
+            delete table;
+        }
+        void newdata(Data d) {
+            // will change the table to the newest form
+        }
+        bool still_alive(void) {
+            return status;
+        }
+        void show_table {
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++)
+                    cout << table[i][j] << ' ';
+            }
+            
+        }
+}
+
 int main(void) {
-    fstream input, output;
-    char buffer[300];
+    int m, n;
+    int i = 0, j = 0, k = 0;
+    struct Data data[1001]; 
 
-    input.open("tetris.data", ios::in);
-    output.open("tetris.final", ios::out);
+    cin >> m >> n;
 
-    if (!input) {
-        cout << "can't open the file\n";
-
+    while (1) {
+        cin >> data[j].type;
+        if((strcmp(data[j].type, "End") == 0))
+            break;
+        cin >> data[j].column;
+        j++;
     }
-    else {
-        input.read(buffer, sizeof(buffer));
+    game tetris(m, n);
+    while (tetris.still_alive() && i < j) {
+        tetris.newdata(data[i++]);
     }
-    cout << buffer<<endl;
-    return 0;
     
+    
+
+    return 0;    
 }
